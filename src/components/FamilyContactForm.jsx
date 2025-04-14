cecb22156d85d11a7d24cfffa1c6b3765e3bf298
Saviskar <Saviskar>
2025-04-14 10:48:57 +0530

    create resident contact feature without resident id param

--- src/components/FamilyContactForm.jsx
+++ src/components/FamilyContactForm.jsx
@@ -1,15 +1,61 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import FormInput from "./FormInput";
 import Home from "../pages/Home";
+import axios from "axios";
 
 function FamilyContactForm() {
+  const [values, setValues] = useState({
+    // First Contact
+    firstContactSurname: "",
+    firstContactGivenName: "",
+    firstContactAddress: "",
+    firstContactPostcode: "",
+    firstContactTelDaily: "",
+    firstContactTelAfterhours: "",
+    firstContactRelationship: "",
+    firstContactEmail: "",
+
+    // Second Contact
+    secondContactSurname: "",
+    secondContactGivenName: "",
+    secondContactAddress: "",
+    secondContactPostcode: "",
+    secondContactTelDaily: "",
+    secondContactTelAfterhours: "",
+    secondContactRelationship: "",
+    secondContactEmail: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const url = "http://localhost:3000/residents/create";
+
+    axios
+      .post(url, values)
+      .then((res) => {
+        console.log(res);
+        navigate("/guardianContact");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.id]: [e.target.value] });
+  };
+
   return (
     <>
       <h2 className="text-xl font-semibold mb-6 text-gray-700">
         Family Contact Details
       </h2>
-      <form className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <form
+        onSubmit={handleSubmit}
+        className="grid grid-cols-1 md:grid-cols-2 gap-5"
+      >
         {/* First Contact */}
         <h3 className="md:col-span-2 text-lg font-medium text-gray-600">
           First Contact
@@ -18,45 +64,61 @@ function FamilyContactForm() {
         <FormInput
           label="Surname"
           id="firstContactSurname"
+          value={values.firstContactSurname}
           placeholder="Enter surname"
+          onChange={handleChange}
         />
         <FormInput
           label="Given Name"
           id="firstContactGivenName"
+          value={values.firstContactGivenName}
           placeholder="Enter given name"
+          onChange={handleChange}
         />
         <FormInput
           label="Address"
           id="firstContactAddress"
+          value={values.firstContactAddress}
           placeholder="Enter address"
+          onChange={handleChange}
         />
         <FormInput
           label="Postcode"
           id="firstContactPostcode"
+          value={values.firstContactPostcode}
           placeholder="Enter postcode"
+          onChange={handleChange}
         />
         <FormInput
           label="Telephone Daily"
           id="firstContactTelDaily"
+          value={values.firstContactTelDaily}
           type="tel"
           placeholder="Enter daily phone"
+          onChange={handleChange}
         />
         <FormInput
           label="Telephone Afterhours"
           id="firstContactTelAfterhours"
+          value={values.firstContactTelAfterhours}
           type="tel"
           placeholder="Enter afterhours phone"
+          onChange={handleChange}
         />
         <FormInput
           label="Relationship to Guest"
           id="firstContactRelationship"
+          value={values.firstContactRelationship}
           placeholder="Enter relationship"
+          onChange={handleChange}
         />
         <FormInput
           label="Email Address"
           id="firstContactEmail"
+          value={values.firstContactEmail}
           type="email"
           placeholder="Enter email"
+          onChange={handleChange}
         />
 
         {/* Second Contact */}
@@ -67,45 +129,61 @@ function FamilyContactForm() {
         <FormInput
           label="Surname"
           id="secondContactSurname"
+          value={values.secondContactSurname}
           placeholder="Enter surname"
+          onChange={handleChange}
         />
         <FormInput
           label="Given Name"
           id="secondContactGivenName"
+          value={values.secondContactGivenName}
           placeholder="Enter given name"
+          onChange={handleChange}
         />
         <FormInput
           label="Address"
           id="secondContactAddress"
+          value={values.secondContactAddress}
           placeholder="Enter address"
+          onChange={handleChange}
         />
         <FormInput
           label="Postcode"
           id="secondContactPostcode"
+          value={values.secondContactPostcode}
           placeholder="Enter postcode"
+          onChange={handleChange}
         />
         <FormInput
           label="Telephone Daily"
           id="secondContactTelDaily"
+          value={values.secondContactTelDaily}
           type="tel"
           placeholder="Enter daily phone"
+          onChange={handleChange}
         />
         <FormInput
           label="Telephone Afterhours"
           id="secondContactTelAfterhours"
+          value={values.secondContactTelAfterhours}
           type="tel"
           placeholder="Enter afterhours phone"
+          onChange={handleChange}
         />
         <FormInput
           label="Relationship to Guest"
           id="secondContactRelationship"
+          value={values.secondContactRelationship}
           placeholder="Enter relationship"
+          onChange={handleChange}
         />
         <FormInput
           label="Email Address"
           id="secondContactEmail"
+          value={values.secondContactEmail}
           type="email"
           placeholder="Enter email"
+          onChange={handleChange}
         />
 
         {/* Submit Buttons */}
